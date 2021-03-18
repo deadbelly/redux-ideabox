9f15a118f448788f8a22d17394c55a7d9786b1ea
Boone Epstein <booneepstein@gmail.com>
2021-03-18 17:25:05 -0500

    Move AddTodoForm

---
+++ src/containers/AddTodoForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { addToDo } from '../actions';
+import { connect } from 'react-redux';
+
+class AddTodoForm extends Component {
+
+  submitForm = (e) => {
+    e.preventDefault()
+    this.props.addToDo(this.state.todo)
+    this.setState({ todo: '' });
+  }
+
+  render() {
+    return (
+      <section>
+        <form onSubmit={this.submitForm}>
+          <input
+            value={this.state.todo}
+            placeholder="Add A Todo"
+            onChange={this.handleChange} />
+          <button>Add Todo</button>
+        </form>
+      </section>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch( addToDo(text) )
+})
+
+export default connect(null, mapDispatchToProps)(AddTodoForm);
