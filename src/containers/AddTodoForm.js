7e2deb01fda126c8530b0421ca7c014afa59ea2b
Boone Epstein <booneepstein@gmail.com>
2021-03-18 19:45:31 -0500

    Rewrite constructor and change handler

--- src/containers/AddTodoForm.js
+++ src/containers/AddTodoForm.js
@@ -3,6 +3,14 @@ import { addToDo } from '../actions';
 import { connect } from 'react-redux';
 
 class AddTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todo: '' };
+  }
+
+  handleChange = (e) => {
+    this.setState({ todo: e.target.value });
+  }
 
   submitForm = (e) => {
     e.preventDefault()
