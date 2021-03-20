9e321e8a3fb296eb9aca8809acaa213695578319
Boone Epstein <booneepstein@gmail.com>
2021-03-19 20:11:17 -0500

    Edit reducer syntax

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -2,14 +2,14 @@ export const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [...state, { id: Date.now(), text: action.todo, completed: false }];
-     case 'TOGGLE_COMPLETE':
+    case 'TOGGLE_COMPLETE':
       const toggledState = state.map(todo => {
         if (todo.id === action.id) {
-          todo.completed = !todo.completed
+          todo.completed = !todo.completed;
         }
-        return todo
+        return todo;
       });
-      return toggledState
+      return toggledState;
     default:
       return state;
   }
